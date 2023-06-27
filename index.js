f9430ca579e7f432e7f71efe6d126f3ed591d280
Cody Lakin <codylakin474@gmail.com>
2023-06-27 12:49:08 -0500

    deployment test

--- index.js
+++ index.js
@@ -4,13 +4,30 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the current directory
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const port = 3000;
+
+// // Serve static files from the "public" directory
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
