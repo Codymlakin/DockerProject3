88491bed9de26c1e187860dbbfb01323639d29df
Cody Lakin <codylakin474@gmail.com>
2023-06-27 12:56:02 -0500

    deployment test

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the current directory
-app.use(express.static(__dirname));
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  const indexPath = path.join(__dirname, 'index.html');
+  res.sendFile(indexPath);
 });
 
 app.listen(port, () => {
